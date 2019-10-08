954156cfdac4bdab17b187c941c86314fae6405c
Toxcity <32424628+ToxcityDev@users.noreply.github.com>
2019-10-07 20:42:00 -0400

    Create UtilsAPI.java

---
+++ UtilsAPI.java
@@ -0,0 +1,29 @@
+package me.toxcity.qlib;
+
+public class UtilsAPI {
+	
+	
+	public String Creator() {
+		String creator = "Toxcity";
+		return creator;
+	}
+	public String Verion() {
+		String version = "V.1";
+		return version;
+	}
+	public String Snowball() {
+		String snowball = "Snowball";
+		return snowball;
+	}
+	public String Grapple() {
+		String grapple = "Grapple";
+		return grapple;
+	}
+	public String Pearls() {
+		String fastpearl = "Fast Pearl";
+		return fastpearl;
+	}
+	
+	
+
+}
